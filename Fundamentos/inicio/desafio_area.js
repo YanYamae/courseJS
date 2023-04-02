b59718acb2aae9dd91adcb39b52137e4ec9787cb
YanYamae <coelho.ycy@gmail.com>
2023-04-02 16:29:23 +0200

    Aula 32

---
+++ Fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// PI 3.14
+// area = PI * raio * raio
+
+const PI = 3.14
+let raio = 10
+
+let res = PI * (raio * raio)
+
+
+console.log("Area = " + res + "m²")
+console.log("PI: " + Math.PI)
---
+++ Fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,13 @@
+let A = 7
+let B = 94
+
+// trocar o valor das variaveis entre si ---> A = 94 e B = 7
+
+let C = A
+A = B
+B = C
+
+// [A , B] = [B , A] isso deveria funcionar mas sei la...
+
+console.log(A)
+console.log(B)
