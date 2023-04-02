2e4a4cb251c4df26c6708211f885afbec30c4bf9
YanYamae <coelho.ycy@gmail.com>
2023-04-02 14:59:19 +0200

    first commit

---
+++ Fundamentos/inicio/dados_4.js
@@ -0,0 +1,11 @@
+//let a = 3
+const a = 3
+
+// ...
+
+//a = 10
+// a = a + 10 não pode ser executado pois o numero é uma constante
+
+// ...
+
+console.log(a)
---
+++ Fundamentos/inicio/sentenca_2.js
@@ -0,0 +1,3 @@
+console.log('Passo #01') //pode ser usado aspas dublas ou simples para printar uma string
+console.log('Passo #02')
+console.log('Passo #03')
