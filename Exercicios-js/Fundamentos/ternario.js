85917eabf464dec5e5c848d83344e7496a820fa3
YanYamae <coelho.ycy@gmail.com>
2023-04-18 20:44:23 +0200

    Aula 72-73

---
+++ Exercicios-js/Fundamentos/ternario.js
@@ -0,0 +1,4 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1)) // desntro da parenteses retorna um valor paro o objeto nota
+console.log(resultado(6.7))
